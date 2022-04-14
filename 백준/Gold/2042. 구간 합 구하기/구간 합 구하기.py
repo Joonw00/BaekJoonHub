f0cfe065a4eacdc94c0ba4b15b0bb79d86a11ef9
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2022-04-14 21:03:23 +0900

    [Gold I] Title: 구간 합 구하기, Time: 572 ms, Memory: 134332 KB -BaekjoonHub

---
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -0,0 +1,49 @@
+#강제로 완전 이진 트리화 시킨다.(0대입)
+import sys
+import math
+global ans
+#1번 연산
+def change_num(idx,c):
+  dt = c - seg[idx]
+  while idx != 0:
+    seg[idx] += dt
+    idx = (idx-1)//2
+  seg[0]+=dt
+#2번 연산
+def sum_range(search_f,search_l,idx_f,idx_l,idx):
+  global ans
+  mid = (idx_f + idx_l)//2
+  if search_f<=idx_f and search_l>=idx_l:
+    ans+=seg[idx]
+    return
+  if mid >= search_f: #좌측 노드로
+    sum_range(search_f,search_l,idx_f,mid,idx*2+1)
+  if mid < search_l: #우측 노드로
+    sum_range(search_f,search_l,mid+1,idx_l,idx*2 + 2)
+  
+
+N,M,K = map(int,sys.stdin.readline().split())
+n = math.ceil(math.log2(N))
+size = 2**n
+tot_size = size
+#세그먼트 초기화
+seg = [0] * (size*2-1)
+for i in range(N):
+  temp = sys.stdin.readline().rstrip()
+  temp = int(temp)
+  seg[size-1 + i] = temp
+#size-1:왼끝
+while True:
+  size = size//2
+  for i in range(size-1,size*2 -1):
+    seg[i] = seg[2*i + 1]+seg[2*i + 2]
+  if size == 0:
+    break
+for i in range(M+K):
+  a,b,c = map(int,sys.stdin.readline().split())
+  if a == 1:
+    change_num(tot_size - 2 + b,c)
+  elif a == 2:
+    ans = 0
+    sum_range(tot_size - 2 + b, tot_size - 2 + c,tot_size - 1,tot_size * 2 - 2,0)
+    print(ans)
