68d5a02a42de6db11f21b39713066577c6a24c91
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2022-04-14 21:18:04 +0900

    Update 작은 새.py

--- 백준/Diamond/10129. 작은 새/작은 새.py
+++ 백준/Diamond/10129. 작은 새/작은 새.py
@@ -5,9 +5,9 @@ n = int(n)
 tr = list(map(int, sys.stdin.readline().split()))
 q = input()
 q = int(q)
-#입력
+
 for i in range(q):
-  #새의 크기
+  #나는 거리
   k = sys.stdin.readline().rstrip()
   k = int(k)
   #[피로도,h,위치]
@@ -21,14 +21,14 @@ for i in range(q):
     
     h = tr[i]
     idx = i
-    #불리한 경우에 대한 조건 설정
+    #의미 없는 정보들 pop
     while dp[-1][0] >= tired:
       if dp[-1][0] == tired and dp[-1][1]>=h:
         break
       dp.pop()
-    #데이터 추가
+
     dp.append([tired,h,i])
-    #sliced
+    #slice
     if i>= dp[0][2]+k:
       dp.popleft()
   print(dp[-1][0])
