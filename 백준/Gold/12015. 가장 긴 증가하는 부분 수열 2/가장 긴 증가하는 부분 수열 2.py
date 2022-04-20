137e012cbfccde8bba81fd1aaec2f03846cd3f21
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2022-04-20 23:19:58 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 540 ms, Memory: 253264 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -0,0 +1,22 @@
+import sys
+N = input()
+N = int(N)
+seq = list(map(int,sys.stdin.readline().split()))
+
+dp = []
+dp.append(0)
+for i in range(N):
+  if seq[i] > dp[-1]:
+    dp.append(seq[i])
+  else:
+    f = 0
+    l = len(dp)
+    while f<l:
+      mid = (f+l)//2
+      #이분 탐색
+      if dp[mid] < seq[i]:
+        f = mid+1
+      else:
+        l = mid
+    dp[l] = seq[i]
+print(len(dp) - 1)
