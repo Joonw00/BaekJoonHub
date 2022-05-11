072a302942f52d693fff7984a178660cdf558262
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2022-05-11 10:56:23 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 5144 ms, Memory: 261320 KB -BaekjoonHub

---
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -0,0 +1,29 @@
+import sys
+def fnd(x):
+  if x!=par[x]:
+    par[x] = fnd(par[x])
+  return par[x]
+
+v,e = map(int,sys.stdin.readline().split())
+par = []
+for i in range(v+1):
+  par.append(i)
+line = []
+for i in range(e):
+  a,b,c = map(int,sys.stdin.readline().split())
+  line.append([c,a,b])
+line.sort()
+
+ans  = 0
+de = 0
+#모든 간선들에 대하여
+for i in range(e):
+  s = fnd(line[i][1])
+  f = fnd(line[i][2])
+  cost = line[i][0]
+  #처음과 같지 않다면 연결
+  if s != f:
+    par[s] = f
+    ans += cost
+    de = max(de,cost)
+print(ans-de)
