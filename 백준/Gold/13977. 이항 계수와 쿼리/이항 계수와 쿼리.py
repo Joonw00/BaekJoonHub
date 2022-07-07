387e77ef694685b2a01aaf7a2b822b6bb144d5dc
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2022-07-08 00:51:55 +0900

    [Gold I] Title: 이항 계수와 쿼리, Time: 660 ms, Memory: 210408 KB -BaekjoonHub

---
+++ 백준/Gold/13977. 이항 계수와 쿼리/이항 계수와 쿼리.py
@@ -0,0 +1,32 @@
+import sys
+INF = 1000000007
+m = input()
+m = int(m)
+def sqr(a,b):
+    #종료 조건
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a
+    temp = sqr(a,b//2)
+    if b%2 == 1:
+        return temp*temp*a % INF
+    else:
+        return temp*temp%INF
+fact = []
+fact.append(1)
+fact.append(1)
+for i in range(2,4000000+1):
+    temp = fact[-1]
+    temp = (temp*i)%INF
+    fact.append(temp)
+for i in range(m):
+    n,k = map(int,sys.stdin.readline().split())
+    up = fact[n] #분자
+    und = fact[k] * fact[n-k] #분모
+    und = und%INF #??
+
+    rev = sqr(und,INF-2)
+    rev = rev%INF
+    ans = (up*rev)%INF
+    print(ans)
