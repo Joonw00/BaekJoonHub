5221da6348481a825c9c7727eb56b881bbd59cd8
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2022-07-08 09:09:53 +0900

    [Gold I] Title: 서로소, Time: 112 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Gold/4355. 서로소/서로소.py
@@ -0,0 +1,17 @@
+import sys
+while True:
+    n = sys.stdin.readline().rstrip()
+    n = int(n)
+    if n == 0:
+        break
+    ans = n
+    #에라토~ 체
+    for i in range(2,int(n**(1/2))+1):
+        if n%i == 0:
+            ans = ans * (1 - 1/i)
+        while n%i == 0:
+            n = n // i
+    #소수라면
+    if n>1:
+        ans = ans* (1-1/n)
+    print(int(ans))
