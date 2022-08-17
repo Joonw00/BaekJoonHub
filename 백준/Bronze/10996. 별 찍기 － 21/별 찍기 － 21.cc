d8c1ef71500fa2a4c528ae3aac5e90bdaae4da0f
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2022-08-17 21:23:25 +0900

    [Bronze II] Title: 별 찍기 - 21, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10996. 별 찍기 － 21/별 찍기 － 21.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin>>n;
+	for(int i = 0; i<2*n; i++){
+		if(i%2 == 0){ // 첫줄 
+			for(int j = 0; j<n;j++){
+				if(j%2 == 0) cout<<'*';
+				else cout<<" ";
+			}
+			cout<<endl;
+		}
+		else{
+			for(int j = 0;j<n;j++){
+				if(j%2 == 0) cout<<" ";
+				else cout<<"*";
+			}
+			cout<<endl;
+		}
+	}
+	return 0;
+} 
