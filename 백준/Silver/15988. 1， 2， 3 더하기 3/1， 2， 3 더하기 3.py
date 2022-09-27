653795da0006fcfb9481e663f2f1c36940726599
Joonw00 <97094633+Joonw00@users.noreply.github.com>
2022-09-28 02:05:47 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 128 ms, Memory: 122224 KB -BaekjoonHub

---
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.py
@@ -0,0 +1,13 @@
+import sys
+t = input()
+t = int(t)
+dp = [0] * 1000001
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in range(4,1000001):
+    dp[i] = (dp[i-1] + dp[i-2] + dp[i-3]) % 1000000009
+for i in range(t):
+    n = sys.stdin.readline().rstrip()
+    n = int(n)
+    print(dp[n])
