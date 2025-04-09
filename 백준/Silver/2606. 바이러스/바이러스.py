ad3260437a5e1865ed1741c6327ce1036b7af7b8
박준우 <97094633+Joonw00@users.noreply.github.com>
2025-04-09 18:47:26 +0900

    [Silver III] Title: 바이러스, Time: 116 ms, Memory: 113500 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,33 +1,35 @@
 import sys
-N = input()
-N = int(N)
-p = input()
-p = int(p)  #p쌍
-pair = []
-for _ in range(p):
-    a,b = map(int, sys.stdin.readline().split())
-    pair.append([a,b])
-pair.sort()
-#입력받고 정렬
+from collections import deque
+
+n = int(input())
+
+pair = int(input())
+
+adj = [[] for _ in range(n+1)]
+for i in range(pair):
+    a,b = map(int,sys.stdin.readline().split())
+
+    adj[a].append(b)
+    adj[b].append(a)
+
+visited = [0 for _ in range(n+1)]
 
 
-vir = []
-#모체 설정
-vir.append(1)
-n = 0
-
-while len(vir)> n :
-    for i in range(p):
-        if vir[n] == pair[i][0] and pair[i][1] not in vir:
-            vir.append(pair[i][1])
-        #뒤의 컴퓨터가 바이러스, 앞의 것이 아직 걸린 적 없을 때
-        elif vir[n] == pair[i][1] and pair[i][0] not in vir:
-            vir.append(pair[i][0])
-    n+=1
-
-#에러 뜬 이유:2-1일 때, 2는 바이러스에 감염되는데, 고려 안함
-
-
-#1제외
-print(len(vir)-1)
-
+q = deque()
+for i in range(len(adj[1])):
+    q.append(adj[1][i])
+visited[1] = 1
+while q:
+    node = q.popleft()
+    visited[node] = 1
+    l = len(adj[node])
+    for i in range(l):
+        nextNode = adj[node][i]
+        if visited[nextNode] == 1:
+            continue
+        q.append(nextNode)
+ans = 0
+for i in range(n+1):
+    if visited[i] == 1:
+        ans+=1
+print(ans-1)
