ad0440099714aa1231301b4b7af1afedfaa95404
박준우 <97094633+Joonw00@users.noreply.github.com>
2025-04-19 18:59:05 +0900

    [level 2] Title: 더 맵게, Time: 1675.66 ms, Memory: 48.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,17 @@
+import heapq
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    while scoville[0] < K:
+        mix = heapq.heappop(scoville) + heapq.heappop(scoville) * 2
+        
+        heapq.heappush(scoville, mix)
+        answer+=1
+        
+        
+        if len(scoville) == 1 and scoville[0] < K:
+            answer = -1
+            break
+    
+    return answer
